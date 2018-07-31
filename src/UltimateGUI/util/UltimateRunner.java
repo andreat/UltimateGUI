70ca501140fe58c0eb2dee7d76869645531558eb
Andrea Turrini <andrea.turrini@gmail.com>
2018-07-31 10:12:14 +0800

    In-house byte[] to hex conversion

--- src/UltimateGUI/util/UltimateRunner.java
+++ src/UltimateGUI/util/UltimateRunner.java
@@ -28,12 +28,20 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
-import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
-
 public class UltimateRunner {
+	
+	private final static char[] hexArray = "0123456789abcdef".toCharArray();
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for (int j = bytes.length - 1; j >= 0; j--) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
 	
 	public enum ARCHITECTURE {
 		_32BIT ("32bit"),
@@ -305,7 +313,7 @@ public class UltimateRunner {
 	            sha1.update(buffer, 0, len);
 	            len = input.read(buffer);
 	        }
-	        return new HexBinaryAdapter().marshal(sha1.digest()).toLowerCase(Locale.ROOT);
+	        return bytesToHex(sha1.digest());
 		}
 	}
 
